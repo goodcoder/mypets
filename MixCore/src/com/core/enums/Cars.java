c352620306fbac0e5d35141b996fd7c39c09a2c9
BobPc <BobPc@bob.T-mobile.com>
2018-01-14 02:22:05 -0500

    updated

--- MixCore/src/com/core/enums/Cars.java
+++ MixCore/src/com/core/enums/Cars.java
@@ -5,6 +5,9 @@ public class Cars {
 	public static void main(String[] args) {
 		MyCars[] cars = MyCars.values();
 		System.out.println(cars[0]);
+		
+		MyCars HONDA = MyCars.HONDA;
+		HONDA.doSomeProcess();
 	}
 
 }
@@ -20,6 +23,10 @@ enum MyCars {
 
 	public String getModel() {
 		return this.model;
+	}
+	
+	public void doSomeProcess(){
+		System.out.println("Do something else.... within ENUM like any regular business logic!!!");
 	}
 
 }
